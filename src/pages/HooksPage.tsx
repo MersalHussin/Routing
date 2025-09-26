4fb03e19004c662d73ffe6a9d46f1cd9a8d7e3d6
mersalhussin <mersalhussin@gmail.com>
2025-09-26 03:03:48 +0300

    اتعلمنا ان الـ Use Effect ده بديل لـ الـ Component Life Cicyle أللي كانت بتحصل في الـ Class لكن هنا انت بتستخدم الـ Use Effect عشان تسبتدلهم وتخلي  كل حاجه تمام والـ الـ Dependency List هي لو سبتها فاضيه فهي بتشغل الـ Array لما تبقى Mount بس أول مرة ولو شلتها هتعمل DidUpdate كل تحديث ولو كتبت فيها قيمة معينة او State معينة هتحصل فيها DidUpdate لو عملت تحديث على الـ State دي بس
    ولو مش عاوز حاجه تسمع بحطها في Use Effect تانية زي الـ API مثلًا

--- src/pages/HooksPage.tsx
+++ src/pages/HooksPage.tsx
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
+// اتعلمنا ان الـ Use Effect ده بديل لـ الـ Component Life Cicyle أللي كانت بتحصل في الـ Class 
+// لكن هنا انت بتستخدم الـ Use Effect عشان تسبتدلهم وتخلي  كل حاجه تمام
+// والـ الـ Dependency List هي لو سبتها فاضيه فهي بتشغل الـ Array لما تبقى Mount بس أول مرة 
+// ولو شلتها هتعمل DidUpdate كل تحديث 
+// ولو كتبت فيها قيمة معينة او State معينة هتحصل فيها DidUpdate لو عملت تحديث على الـ State دي بس 
 
+// ولو مش عاوز حاجه تسمع بحطها في Use Effect تانية زي الـ API مثلًا
 
 const HooksPage = () => {
     const [counter, setcounter] = useState(0);
