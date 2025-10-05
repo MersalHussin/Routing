c91a168447e62ad36692d15327ead77fc7ed5e8f
mersalhussin <mersalhussin@gmail.com>
2025-10-05 18:50:00 +0300

    Navigate State And Replace

--- src/components/auth/PrtectedRoute.tsx
+++ src/components/auth/PrtectedRoute.tsx
@@ -7,13 +7,14 @@ import { Navigate } from "react-router-dom";
         isAllowd: boolean;
         redireactPath: string;
         children: ReactNode;
+        data?: unknown;
         }
 
-        const PrtectedRoute = ({isAllowd,redireactPath , children}:IProps) => {
+        const PrtectedRoute = ({isAllowd,redireactPath , data,children}:IProps) => {
 
             //** TODO Replace && State
            if(!isAllowd){
-            return <Navigate to={redireactPath}/>
+            return <Navigate to={redireactPath} state={data}/>
            }else{
             return children
            }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,68 +1,69 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
-import HomePage from "../pages/HomePage";
-import Aboutus from "../pages/AboutUsPage";
-import Products from "../pages/ProductsPage";
-import RootLayout from "../pages/Layout";
-import QuickStartPage from "../pages/learn";
-import LearnLayout from "../pages/learn/LearnLayout";
-import ThinkingInReact from "../pages/learn/ThinkingInReact";
-import Installiton from "../pages/learn/Installiton";
-import Login from "../pages/Login";
-import Contribute from "../pages/Contribute";
-import PrtectedRoute from "../components/auth/PrtectedRoute";
+  import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+  } from "react-router-dom";
+  import HomePage from "../pages/HomePage";
+  import Aboutus from "../pages/AboutUsPage";
+  import Products from "../pages/ProductsPage";
+  import RootLayout from "../pages/Layout";
+  import QuickStartPage from "../pages/learn";
+  import LearnLayout from "../pages/learn/LearnLayout";
+  import ThinkingInReact from "../pages/learn/ThinkingInReact";
+  import Installiton from "../pages/learn/Installiton";
+  import Login from "../pages/Login";
+  import Contribute from "../pages/Contribute";
+  import PrtectedRoute from "../components/auth/PrtectedRoute";
 
 
-const isLoggedIn = true;
+  const isLoggedIn = true;
+  const userData = isLoggedIn ? { name: "John Doe", email: "mersalhussin"}: null;
 
-const router = createBrowserRouter(
-   createRoutesFromElements(
-<>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+  <>
 
-      {/* Root Layout */}
-      {/* الـRoot ده معناه اي حاجه بعد / هيتبق عليها الـ RootLayout */}
-      <Route element={
-        <>
-          <RootLayout />
-      
-          </> } path="/">
-        <Route index element= {<HomePage/>} />,
-        <Route element={<Aboutus/>} path="about"/>,
-        <Route element={<Products/>} path="products"/>,
+        {/* Root Layout */}
+        {/* الـRoot ده معناه اي حاجه بعد / هيتبق عليها الـ RootLayout */}
         <Route element={
-          <PrtectedRoute isAllowd={!isLoggedIn} redireactPath="/contribute">
-            <Login/>
-          </PrtectedRoute>
+          <>
+            <RootLayout />
+        
+            </> } path="/">
+          <Route index element= {<HomePage/>} />,
+          <Route element={<Aboutus/>} path="about"/>,
+          <Route element={<Products/>} path="products"/>,
+          <Route element={
+            <PrtectedRoute data={userData} isAllowd={!isLoggedIn} redireactPath="/contribute">
+              <Login/>
+            </PrtectedRoute>
 
-        } path="login"/>,
-        <Route element={
-          <PrtectedRoute isAllowd={isLoggedIn} redireactPath="/login">
-            <Contribute/>
-          </PrtectedRoute>
-      
-      } path="contribute"/>,
-      </Route>
-
-
-      {/* Learn Layout */}
-      <Route element={<LearnLayout />} path="/learn">
-           <Route index element={<QuickStartPage/>} path="quickstart" />,
-           <Route  element={<ThinkingInReact/>} path="thinking" />,
-           <Route  element={<Installiton/>} path="installition" />,
-      </Route>
-</>
-));
+          } path="login"/>,
+          <Route element={
+            <PrtectedRoute  isAllowd={isLoggedIn} redireactPath="/login">
+              <Contribute/>
+            </PrtectedRoute>
+        
+        } path="contribute"/>,
+        </Route>
 
 
-export default router;
+        {/* Learn Layout */}
+        <Route element={<LearnLayout />} path="/learn">
+            <Route index element={<QuickStartPage/>} path="quickstart" />,
+            <Route  element={<ThinkingInReact/>} path="thinking" />,
+            <Route  element={<Installiton/>} path="installition" />,
+        </Route>
+  </>
+  ));
 
 
-/**
-  * Layout.tsx
-  * Page Component => index.tsx => Home Page
-  * Page Component => index.tsx => About Page
-  * Page Component => index.tsx => Products Page
-  * **/
+  export default router;
+
+
+  /**
+    * Layout.tsx
+    * Page Component => index.tsx => Home Page
+    * Page Component => index.tsx => About Page
+    * Page Component => index.tsx => Products Page
+    * **/
