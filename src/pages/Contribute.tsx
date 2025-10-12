7ab504f044ac7a1a9f5520fafe613fdf02c3d058
mersalhussin <mersalhussin@gmail.com>
2025-10-12 19:35:58 +0300

    Before Error Element

--- src/pages/Contribute.tsx
+++ src/pages/Contribute.tsx
@@ -1,16 +1,15 @@
-import React from "react";
 import Textarea from "../ui/Textarea";
 import Button from "../ui/Button";
-import { useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const Contribute = () => {
     const {state} = useLocation();
-    console.log(state);
   return (
     <div className="min-h-[calc(100vh-92px)] flex items-center justify-center bg-[#23272f] text-gray-100 px-4">
       <div className="bg-[#2c313a]/90 backdrop-blur-sm p-8 rounded-2xl shadow-2xl w-full max-w-lg">
         <h1 className="text-3xl font-bold text-center text-[#61dafb] mb-2">
-            {state ? `Hello ${state.name}` : 'Contribute to Our Project'}
+            {/* {state ? `Hello ${state.name}` : 'Contribute to Our Project'} */}
+            {state ? `Hello ${state.name}` : <Navigate to='/login'/>}
         </h1>
         <p className="text-[#61dafb] text-center mb-4">
           Contribute & Report Issues
